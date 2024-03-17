0d787f78907a14569fce0efaebe59b69cc45bc73
saagor16 <saagor.16@gmail.com>
2024-03-17 11:14:31 +0600

    img pblm solve

--- src/container/Banner/Banner.jsx
+++ src/container/Banner/Banner.jsx
@@ -8,9 +8,9 @@ const Banner = () => {
                 <h2 className='lg:text-4xl font-bold pt-8'>Online Cooking Classes
                 Recipe Calories here <br /> to make  cooking simple for you!</h2>
                 <p className='pt-2 text-xl '>Join Recipe Calories for an immersive and interactive online cooking experience via Zoom, right from the comfort of your <br /> own home.Cook with  step through easy-to-follow Recipe Calories recipes.</p>
-                <div className='lg:space-x-10 pt-5 pb-4 space-x-4'>
+                <div className='lg:space-x-10 pt-5 pb-4 space-x-4 '>
                     <button className="btn  bg-green-700 rounded-3xl text-black border-none lg:w-40">Explore Now</button>
-                    <button className="btn btn-outline  btn-success  rounded-3xl lg:w-40">Our Feedback</button>
+                    <button className="btn btn-outline  btn-success border-white  rounded-3xl lg:w-40"><span className='text-white'>Our Feedback</span></button>
                 </div>
             </div>
           </div>
